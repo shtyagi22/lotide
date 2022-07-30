f97885e917c7719daf8d9a65e28b252eb5e8f9d2
shtyagi22 <sh.tyagi22@gmail.com>
2022-07-30 18:43:32 -0400

    countLetters excercise

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`)
+  }
+  else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+const countLetters = function (str1) {
+  const result = {}
+  for (item of str1) {
+    if (result[item] === "\s+") {
+      //do nothing
+    }
+    else {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1
+      }
+    }
+
+  }
+  return result;
+
+}
+
+console.log(countLetters("lighthouse in the house")) 
