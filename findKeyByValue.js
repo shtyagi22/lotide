9e343664dbd2e113dcb6a3a693730591bff32b05
shtyagi22 <sh.tyagi22@gmail.com>
2022-07-30 20:35:39 -0400

    find key by value in objects

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`)
+  }
+  else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+const findKeyByValue = function (obj, value) {
+  for (const key of Object.keys(obj)) {
+
+    if (obj[key] === value) {
+      return key
+
+    }
+  }
+
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+console.log((findKeyByValue(bestTVShowsByGenre, "The Wire")));
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama");
