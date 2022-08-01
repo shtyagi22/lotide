f572fa8228c88fba6fec21cb8570c078347aaa16
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-01 18:04:04 -0400

    refactoring code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,5 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  }
-  else {
-    for (i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false
-      }
-    }
-    return true
-  }
-}
+const eqArrays = require('./eqArrays');
+
 
 const assertArraysEqual = function (actual, expected) {
   if (actual === expected) {
@@ -22,7 +11,4 @@ const assertArraysEqual = function (actual, expected) {
 }
 
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)// => true
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)// => false
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)// => false
+module.exports = assertArraysEqual;
