ed77d82bc47b947c029739ddea71f8b011da6d4d
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-01 16:23:57 -0400

    refactoring code

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,5 @@ const assertEqual = function (actual, expected) {
     console.log(`Assertion Failed: ${actual} !== ${expected}`)
   }
 };
+module.exports = assertEqual;
 
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(3, 4);
--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+/***const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`)
   }
@@ -6,12 +6,12 @@ const assertEqual = function (actual, expected) {
     console.log(`Assertion Failed: ${actual} !== ${expected}`)
   }
 };
+***/
+const assertEqual = require('./assertEqual');
 
 const head = function (arr1) {
   return arr1[0]
 
 }
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+module.exports = head;
