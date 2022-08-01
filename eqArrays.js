ea39873fc196b79a0f0adfd78902ba3a52e83d31
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-01 17:58:14 -0400

    refactoring code

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
-  }
-  else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
-  }
-};
+
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -25,7 +19,4 @@ const eqArrays = function (arr1, arr2) {
 
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)// => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true)// => false
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-/***const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
-  }
-  else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
-  }
-};***/
-
 const tail = function (arr1) {
   const arr2 = arr1.slice(1)
   return arr2
